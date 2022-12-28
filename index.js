e4db0bfead01304c8ba3b20c7be8b88f680bba2c
Erez <innovaterz@gmail.com>
2022-12-28 12:53:55 +0200

    express routes

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log('hi cicd erez pr2');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+
+app.get("/", async (req, res) => {
+  res.json({ status: "hi procamp" });
+});
+
+
+
+app.get("/time", async (req, res) => {
+  const now = new Date().toString();
+  res.json({ time: now });
+});
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
