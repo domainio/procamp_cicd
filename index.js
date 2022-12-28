786044fdc213301abb7af8668783e3d3070bf28e
Erez <innovaterz@gmail.com>
2022-12-28 17:11:15 +0200

    fix lint

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 8080;
 
 
 app.get("/", async (req, res) => {
@@ -18,4 +18,4 @@ app.get("/time", async (req, res) => {
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
-})
+});
