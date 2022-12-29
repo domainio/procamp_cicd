17a555ae3c5d81a4ca4b330dd5b8ae34b1d94d3b
yakovsinwani <yakovsinwani@gmail.com>
2022-12-29 10:39:17 +0200

    from yakov

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-
+app.use((req, res, next) => {
+  console.log("hello from yakov");
+  next();
+});
 app.get("/", async (req, res) => {
   res.json({ status: "hi procamp team" });
 });
